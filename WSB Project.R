4d708fd472b83431967abd1c334589742ab0bc11
mreyers <matthewreyers3333@gmail.com>
2018-01-16 12:17:45 -0800

    Updated objective

--- WSB Project.R
+++ WSB Project.R
@@ -1,1 +1,5 @@
 # Walking School Bus start file
+
+# Idea: Build a simulation study that ogranically creates data points,
+#       Calls Google API in path mapping, and optimizes number of walking school busses
+#       With minimum requirements on student distance and number of busses
