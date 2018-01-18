f29ed4b0ea53c06d2464ba2ad982bbde012c7e92
mreyers <matthewreyers3333@gmail.com>
2018-01-18 15:09:21 -0800

    Added leader designation to houses

--- WSB Project.R
+++ WSB Project.R
@@ -33,7 +33,6 @@ plottedSchool <- ggmap(ArmstrongSchool) + geom_point(aes(x = -122.9083027, y = 4
 
 # Create a polygon bounding the school catchment
   # Will be done heuristically for now
-# Finish Coordinates, just did Elwell and Fareham, started at Newcombe and 10th
 lon <- c(-122.917433, -122.926703, -122.927647, -122.930436, -122.927475, -122.931520, -122.914654, -122.913066, -122.906414, -122.892510, -122.892682, -122.897660)
 lat <- c(49.225133, 49.232405, 49.231957, 49.234297, 49.235838, 49.239243, 49.249329, 49.247760, 49.247087, 49.240587, 49.235656, 49.235124)
 polyCoords <- as.data.frame(cbind(lon, lat)) 
@@ -48,6 +47,25 @@ P1 = Polygon(coords)
 Ps1 = SpatialPolygons(list(Polygons(list(P1), ID = "a")), proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) # This creates the desired polygon boundary
 
 # Sample n students from the region and add to the plot. Raw data will be used again later for API call
-randomHouses <- spsample(Ps1, n = 10, type = "random")@coords %>% as.data.frame()
+n = 10
+randomHouses <- spsample(Ps1, n = n, type = "random")@coords %>% as.data.frame()
 studentsInCatchment <- plottedSchool + geom_point(data = randomHouses, aes(x = x, y = y))
 studentsInCatchment
+
+# Select some of the random houses to represent parents that are willing to lead the WSB
+  # Do this according to diff probability distributions
+    # Uniform
+    uniVals <- runif(n) #[0, 1]
+    
+    # Normal
+    normVals <- rnorm(n) #(-inf, inf), mean = 0, sd = 1
+    
+    # Log Normal
+    logNVals <- rlnorm(n) # (0, inf), mean = exp^.5 = 1.61
+    
+# Create a systematic way to decide which houses are volunteer leaders    
+houses <- cbind(randomHouses, uniVals, normVals, logNVals)    
+houses$UniLeader <- (houses$uniVals > 0.5)
+houses$normLeader <- (houses$normVals > 0.5)
+houses$logNLeader <- (houses$logNVals > 1.61)
+head(houses)
